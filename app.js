827763d831a5bde8c3c8d6c224af570886b56e68
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2020-03-29 14:22:15 +0200

    Simple enough 🤢

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const router = express.Router();
 
-const model= require('./model.js');
+
  
 
 
@@ -16,13 +16,12 @@ app.get('/',function(req,res){
 app.listen(3000);
 console.log('listening on port 3000');
 
-router.route("/fetch").get(function(req, res) {
-  model.find({}, function(err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
+
+
+
+
+
+
  
-    }
-  });
-});
+ 
+
